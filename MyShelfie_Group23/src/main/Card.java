f90a4811de9e79be66d971fc43d2650882d7907a
sboarinayuri@gmail.com <yurisboarina@MacBook-Air-di-Yuri.local>
2023-03-30 12:32:27 +0200

    Added class Card.java

---
+++ MyShelfie_Group23/src/main/Card.java
@@ -0,0 +1,50 @@
+package main;
+
+public class Card {
+	
+	private final String color;
+	
+	private int id;
+	
+	boolean used;
+	
+	public Card() {
+		
+		color = "";
+		used = false;
+		
+	}
+	
+	public boolean getImage() {
+		
+		boolean img = false;
+		
+		return img;
+		
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	public int getID() {
+		return this.id;
+	}
+	
+	public boolean getUsed() {
+		return this.used;
+	}
+	
+	public boolean setUsed() {
+		
+		if(this.used == false) {
+			
+			this.used = true;
+		
+		}
+		
+		return this.used;
+		
+	}
+
+}
