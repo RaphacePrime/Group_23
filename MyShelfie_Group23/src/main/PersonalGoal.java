45ce32f1d7d0c5da0fad153798a2604937fecf17
S-e-r-g-io <s.pedercini1@studenti.unibg.it>
2023-03-30 12:24:24 +0200

    Added PersonalGoal.java (empty)

---
+++ MyShelfie_Group23/src/main/PersonalGoal.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.util.List;
+
+public class PersonalGoal 
+{
+	
+	private int id;
+	
+	private int scores_taken;
+	
+	private List <Integer> score;
+	
+	
+	public PersonalGoal (int id) 
+	{
+		
+	}
+	
+	String getText () 
+	{
+		return "NULL";
+	}
+	
+	boolean getImage () 
+	{
+		return true;
+	}
+	
+	boolean controlGoal (/*Library lib*/)
+	{
+		return true;
+	}
+	
+	void controlScoreTaken () 
+	{
+		
+	}
+}
