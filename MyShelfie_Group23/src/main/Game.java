3981bf0cb48798688a13f1465b0260dbcffbced3
RaphacePrime <raphael.gemello@gmail.com>
2023-03-30 12:48:36 +0200

    Added Game.java (empty)

---
+++ MyShelfie_Group23/src/main/Game.java
@@ -0,0 +1,62 @@
+package main;
+
+import java.util.List;
+
+public class Game 
+{
+	private List<Card> cards;
+	
+	private int number_of_players;
+	
+	private Player players[];
+	
+	private Player active_player;
+	
+	private CommonGoal common_goals[];
+	
+	private LivingRoom living_room;
+	
+	public Game()
+	{
+		
+	}
+	
+	public void turn()
+	{
+		
+	}
+	
+	public void chooseCard(int x, int y)
+	{
+		
+	}
+	
+	public void generateCards()
+	{
+		
+	}
+	
+	public boolean controlCommonGoals()
+	{
+		return true;
+	}
+	
+	public boolean controlPersonalGoal()
+	{
+		return true;
+	}
+	
+	public void setPersonalGoal()
+	{
+		
+	}
+	
+	public Card getCard()
+	{
+		Card c=new Card();
+		return c;
+	}
+	
+	
+
+}
