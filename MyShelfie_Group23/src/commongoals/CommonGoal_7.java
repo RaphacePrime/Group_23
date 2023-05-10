15e3249c621fac27230d287fa42cfb2150980bf9
RaphacePrime <raphael.gemello@gmail.com>
2023-05-10 11:39:39 +0200

    adding package commongoals and declarations of the classes

---
+++ MyShelfie_Group23/src/commongoals/CommonGoal_7.java
@@ -0,0 +1,24 @@
+package commongoals;
+
+import main.Player;
+
+public class CommonGoal_7 extends CommonGoal{
+
+	public CommonGoal_7(int id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean checkGoal(Player player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void output() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
