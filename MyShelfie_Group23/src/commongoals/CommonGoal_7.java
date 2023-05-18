70009a887273db06fec7c69acfe8f48c1e9f2e30
DanUniBG <d.monteleone2@studenti.unibg.it>
2023-05-18 16:47:28 +0200

    Created CommonGoal #7.

--- MyShelfie_Group23/src/commongoals/CommonGoal_7.java
+++ MyShelfie_Group23/src/commongoals/CommonGoal_7.java
@@ -1,24 +1,45 @@
 package commongoals;
-
+import board.Card;
 import player.Player;
 
 public class CommonGoal_7 extends CommonGoal{
 
-	public CommonGoal_7(int id) {
-		super(id);
-		// TODO Auto-generated constructor stub
-	}
+public CommonGoal_7(int id) {
+super(id);
+}
 
-	@Override
-	public boolean checkGoal(Player player) {
-		// TODO Auto-generated method stub
+@Override
+public boolean checkGoal(Player player) {
+Card[][] matrix = player.getLibrary().getMatrix();
+int count = 0;
+
+for(int i = 0; i<matrix.length -1; i++) {
+
+for(int j = 0; j<matrix[i].length - 1; j++) {
+
+if(matrix[i][j].getColor().equals(matrix[i+1][j].getColor())&&
+		matrix[i][j+1].getColor().equals(matrix[i][j].getColor())&&
+		matrix[i+1][j+1].getColor().equals(matrix[i][j].getColor())) {
+
+	count++;
+		}
+	}
+}
+	if(count>=2) {
+
+		return true;
+	}else {
+
 		return false;
 	}
 
-	@Override
-	public void output() {
-		// TODO Auto-generated method stub
-		
-	}
+
+}
+
+@Override
+public void output() {
+// TODO Auto-generated method stub
+
+}
 
 }
