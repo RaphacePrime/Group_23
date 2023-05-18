af05f46a9d0eaebdc48e5c9084b95474b50d5609
PriiDD <simoprid2@gmail.com>
2023-05-18 17:13:04 +0200

    modifing common_goal8

--- MyShelfie_Group23/src/commongoals/CommonGoal_8.java
+++ MyShelfie_Group23/src/commongoals/CommonGoal_8.java
@@ -1,5 +1,6 @@
 package commongoals;
 
+import board.Card;
 import player.Player;
 
 public class CommonGoal_8 extends CommonGoal{
@@ -20,5 +21,32 @@ public class CommonGoal_8 extends CommonGoal{
 		// TODO Auto-generated method stub
 		
 	}
+	public boolean checkRow(String  row[]) {
+		int i,k;
+		
+		for(i=0;i<5;i++) {
+			for(k=0;k<5;k++) {
+				if((row[i]==row[k])&&(i!=k)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	public boolean checkGoal(Card matrix[][]) {
+		int i , k = 0, count = 0;
+		String row[] = null;
+		for(i = 0; i<6;i++) {
+				row[k]=matrix[k][i].getColor();
+			}
+			if(checkRow(row)) {
+				count++;
+			}
+			if(count == 2) {
+				return true;
+		}
+	
 
+		return false;
+}
 }
