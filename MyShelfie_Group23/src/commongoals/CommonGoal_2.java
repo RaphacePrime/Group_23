be854b2652cd9b88bf03372cb11cce8c145d295b
YuriSboarina <y.sboarina@studenti.unibg.it>
2023-05-18 12:15:42 +0200

    Added methods for common goal 2 & 10

--- MyShelfie_Group23/src/commongoals/CommonGoal_2.java
+++ MyShelfie_Group23/src/commongoals/CommonGoal_2.java
@@ -1,23 +1,57 @@
 package commongoals;
 
+import board.Card;
 import player.Player;
 
 public class CommonGoal_2 extends CommonGoal{
 
 	public CommonGoal_2(int id) {
 		super(id);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean checkGoal(Player player) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		Card[][] matrix = player.getLibrary().getMatrix();	//Matrix of the library of the player
+		
+		int x = 0; //counter x axes
+		int y = 0; //counter y axes
+		boolean completed; //true if the common goal is completed  
+		boolean endCycle = false;
+		int DiagonalCoeff = 1; //if DiagonalCoeff = 1 checks the diagonal else if DiagonalCoeff = -1 checks the reverse diagonal
+		
+		String colorFirstCard; //color of the first card
+		
+		do{
+			
+			completed = true;
+			colorFirstCard = matrix[x][y].getColor();
+			
+			for(int i = 0; i < 5; i++) {	
+				if(matrix[x+i][y+(i*DiagonalCoeff)].getColor() != colorFirstCard){
+					completed = false;
+				}	
+			}
+			
+			if(x == 0) {
+				x++;
+			}else if(x == 1) {
+				x = 0;
+				y = 4;
+				DiagonalCoeff = -1;
+			}else if(x == 1 && y == 4){
+				endCycle = true;
+			}
+			
+		}while(!completed && endCycle);
+		
+		return completed;
 	}
 
 	@Override
 	public void output() {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Five cards of the same colour forming a diagonal.");
 		
 	}
 
