ab4b073a2ed49d1d73017baede9633a61a1ae275
PriiDD <simoprid2@gmail.com>
2023-05-20 15:46:01 +0200

    creating common goal 1

--- MyShelfie_Group23/src/commongoals/CommonGoal_1.java
+++ MyShelfie_Group23/src/commongoals/CommonGoal_1.java
@@ -1,18 +1,13 @@
 package commongoals;
 
+import board.Card;
 import player.Player;
 
 public class CommonGoal_1 extends CommonGoal{
-
+	
 	public CommonGoal_1(int id) {
 		super(id);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public boolean checkGoal(Player player) {
-		// TODO Auto-generated method stub
-		return false;
+		 
 	}
 
 	@Override
@@ -20,5 +15,42 @@ public class CommonGoal_1 extends CommonGoal{
 		// TODO Auto-generated method stub
 		
 	}
-
+	public static int contaCoppie(Card[][] matrix,boolean [][] used, int i , int j) {
+        if (i == matrix[j].length) {
+            return 0;
+        }
+        if(matrix[i][j]==null) {
+        	return 0;
+        }
+        if (j == matrix[i].length) {
+            return contaCoppie(matrix,used,i +1 ,0);
+        }
+        int coppie = contaCoppie(matrix,used,i , j +1);
+        if (j +1 < matrix[i].length && matrix[i][j].getColor() == matrix[i][j +1].getColor() && !used[i][j] && !used[i][j +1]) {
+            used[i][j] = true;
+            used[i][j +1] = true;
+            coppie = Math.max(coppie , contaCoppie(matrix,used,i , j +1) +1);
+            used[i][j] = false;
+            used[i][j +1] = false;
+        }
+        if (i +1 < matrix.length && matrix[i][j].getColor() == matrix[i +1][j].getColor() && !used[i][j] && !used[i +1][j]) {
+        	used[i][j] = true;
+        	used[i +1][j] = true;
+            coppie = Math.max(coppie , contaCoppie(matrix,used,i , j +1) +1);
+            used[i][j] = false;
+            used[i +1][j] = false;
+        }
+        return coppie;
+    }
+	@Override
+	public boolean checkGoal(Player player) {
+		Card[][] matrix = player.getLibrary().getMatrix();
+		boolean used[][] = new boolean[5][6];
+		int coppie;
+		coppie = contaCoppie(matrix,used,0,0);
+		return false;
+	}
 }
+
+
+
