a5ca0be578eb3059592c63cdc491711a71bce1d5
MaXsi044 <m.lupu1@studenti.unibg.it>
2023-05-22 21:26:17 +0200

    insert

--- MyShelfie_Group23/src/player/Library.java
+++ MyShelfie_Group23/src/player/Library.java
@@ -1,26 +1,25 @@
 package player;
 
 import java.util.ArrayList;
-
+import java.util.Scanner;
 import board.Card;
+import board.LivingRoomTile;
 
 public class Library {
 	
-	private int  prova;
-	
-	
 	private Card matrix[][];
-	
-	int initialization_matrix [][] ={ 	{0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0},	};
+
 	
 	public Library()
 	{
+		this.matrix = new Card[6][5];
 		
+		int Card [][] ={ 	{1, 2, 3, 4, 5},
+							{1, 2, 3, 4, 5},
+							{1, 2, 3, 4, 5},
+							{1, 2, 3, 4, 5},
+							{1, 2, 3, 4, 5},
+							{1, 2, 3, 4, 5},	};
 	}
 	
 	public boolean checkFullness()
@@ -29,7 +28,37 @@ public class Library {
 	}
 	
 	public boolean insertInLibrary(ArrayList<Card> chosen, int column)
-	{
+	{		
+		System.out.println("insert a position in which you want to put the card. positions go from 1 to 5");
+		Scanner sc = new Scanner(System.in);
+		int Column = sc.nextInt();
+		for (int i = 0; i < Card.Lenght();i++) {
+			if (i == Card[Column]) {
+				Card[Column] = ArrayLinjst<Card> chosen;
+			}
+			else
+				i++;
+		}
+		
+			
+		
+		/**
+		 * String tileColor = new String();
+		tileColor = tile.getCard().getColor();
+		if(tileColor.equals("yellow")) {
+			System.out.print("\u001B[33m" + "*\t" + "\u001B[0m");
+		}else if(tileColor.equals("pink")) {
+			System.out.print("\u001B[31m" + "*\t" + "\u001B[0m");
+		}else if(tileColor.equals("blue")) {
+			System.out.print("\u001B[34m" + "*\t" + "\u001B[0m");
+		}else if(tileColor.equals("green")) {
+			System.out.print("\u001B[32m" + "*\t" + "\u001B[0m");
+		}else if(tileColor.equals("cyan")) {
+			System.out.print("\u001B[36m" + "*\t" + "\u001B[0m");
+		}else if(tileColor.equals("white")) {
+			System.out.print("\u001B[37m" + "*\t" + "\u001B[0m");
+		*/
+		
 		return true;
 	}
 
