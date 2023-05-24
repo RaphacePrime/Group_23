40b5c3931acf30422e819ded76bf679d2e58f85d
RaphacePrime <raphael.gemello@gmail.com>
2023-05-24 10:57:26 +0200

    Correcting Library.java

--- MyShelfie_Group23/src/player/Library.java
+++ MyShelfie_Group23/src/player/Library.java
@@ -9,19 +9,19 @@ public class Library {
 	
 	private Card matrix[][];
 	private Card card;
-	private final boolean legalTile;
+	//private final boolean legalTile;
 	
-	public Library(boolean legalTile)
+	public Library()
 	{
-		this.legalTile = legalTile;
+		//this.legalTile = legalTile;
 		this.card = null;
 		this.matrix = new Card[6][5];
 				
 	}
-	public boolean isLegalTile() 
+	/*public boolean isLegalTile() 
 	{
 		return legalTile;
-	}
+	}*/
 	
 /**	public boolean isOccupied()
 	{
@@ -44,18 +44,18 @@ public class Library {
 		return true;
 	}
 	
-	public void insertInLibrary(ArrayList<Card> chosen, Card[][] matrix, int column, int row)
+	public boolean insertInLibrary(ArrayList<Card> chosen, int cloumn)
 	{		
 		System.out.println("insert a position in which you want to put the card. positions go from 1 to 5");
 		Scanner sc = new Scanner(System.in);
 		int Column = sc.nextInt();
-		for (row = 6; row > 0; row--) 
+		/*for (row = 6; row > 0; row--) 
 		{
 			if(matrix[Column][row]==null) {
-				matrix[Column][row]= "ArrayList<Card> chosen";  //non so quale sia la funzione che va a prendere le carte dalla board 			
+				//matrix[Column][row]= "ArrayList<Card> chosen";  //non so quale sia la funzione che va a prendere le carte dalla board 			
 			} 
 			
-		}
+		}*/
 		
 		
 		/**for (int row = 0; row < 6;row++) {
@@ -87,6 +87,7 @@ public class Library {
 		}else if(tileColor.equals("white")) {
 			System.out.print("\u001B[37m" + "*\t" + "\u001B[0m");
 		*/
+		return false;
 			
 	}
 
