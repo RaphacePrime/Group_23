fdee9df313f5cfd78fd0424458cab7d3bf19d3a9
PriiDD <simoprid2@gmail.com>
2023-05-30 15:57:46 +0200

    commongoal 1 changes

--- MyShelfie_Group23/src/commongoals/CommonGoal_1.java
+++ MyShelfie_Group23/src/commongoals/CommonGoal_1.java
@@ -12,10 +12,7 @@ public class CommonGoal_1 extends CommonGoal{
 
 	@Override
 	public void output() {
-		System.out.println("Sei gruppi separati formati ciascuno "
-				+ "da due tessere adiacenti dello stesso tipo. "
-				+ "Le tessere di un gruppo possono "
-				+ "essere diverse da quelle di un altro gruppo. ");
+		System.out.println("Six separate groups each consisting of two adjacent tiles of the same type.");
 		
 	}
 	public static int contaCoppie(Card[][] matrix,boolean [][] used, int i , int j) {
@@ -27,14 +24,14 @@ public class CommonGoal_1 extends CommonGoal{
             return contaCoppie(matrix,used,i +1 ,0);
         }
         int coppie = contaCoppie(matrix,used,i , j +1);
-        if (j +1 < matrix[i].length && matrix[i][j].getColor().equals(matrix[i][j +1].getColor()) && !used[i][j] && !used[i][j +1]&&matrix[i][j] != null) {
+        if (j +1 < matrix[i].length && matrix[i][j].getColor().equals(matrix[i][j +1].getColor()) && !used[i][j] && !used[i][j +1]&&matrix[i][j]!=null) {
             used[i][j] = true;
             used[i][j +1] = true;
             coppie = Math.max(coppie , contaCoppie(matrix,used,i , j +1) +1);
             used[i][j] = false;
             used[i][j +1] = false;
         }
-        if (i +1 < matrix.length && matrix[i][j].getColor().equals(matrix[i +1][j].getColor())  && !used[i][j] && !used[i +1][j]&&matrix[i][j] != null) {
+        if (i +1 < matrix.length && matrix[i][j].getColor().equals(matrix[i +1][j].getColor())  && !used[i][j] && !used[i +1][j]&&matrix[i][j]!=null) {
         	used[i][j] = true;
         	used[i +1][j] = true;
             coppie = Math.max(coppie , contaCoppie(matrix,used,i , j +1) +1);
@@ -49,6 +46,9 @@ public class CommonGoal_1 extends CommonGoal{
 		boolean used[][] = new boolean[5][6];
 		int coppie;
 		coppie = contaCoppie(matrix,used,0,0);
+		if(coppie >= 6) {
+			return true;
+		}
 		return false;
 	}
 }
