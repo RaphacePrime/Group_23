d081bccb127b726ff1d733b480684b646e30fe8a
PriiDD <simoprid2@gmail.com>
2023-05-30 16:05:50 +0200

    commongoal 8 changes

--- MyShelfie_Group23/src/commongoals/CommonGoal_8.java
+++ MyShelfie_Group23/src/commongoals/CommonGoal_8.java
@@ -14,31 +14,29 @@ public class CommonGoal_8 extends CommonGoal{
 	public boolean checkGoal(Player player) {
 		int i , k = 0, count = 0;
 		Card[][] matrix = player.getLibrary().getMatrix();
-		String row[] = null;
+		String row[] = new String[6];
 		
-		for(i = 0; i<6;i++) {
-				row[k]=matrix[k][i].getColor();
+		for(k=0;k<6;k++) {
+			for(i = 0; i<5;i++) {
+				row[i]=matrix[k][i].getColor();
 			}
 			if(checkRow(row)) {
 				count++;
 			}
-			if(count == 2) {
+			if(count >= 2) {
 				return true;
+			}
 		}
-	
-
 		return false;
 	}
 
 	@Override
 	public void output() {
-		System.out.println("Due righe formate ciascuna "
-						 + "da 5 diversi tipi di tessere.");
+		System.out.println("Two lines each made up of 5 different types of tiles.");
 		
 	}
 	public boolean checkRow(String  row[]) {
 		int i,k;
-		
 		for(i=0;i<5;i++) {
 			for(k=0;k<6;k++) {
 				if((row[i]==row[k])&&(i!=k)) {
