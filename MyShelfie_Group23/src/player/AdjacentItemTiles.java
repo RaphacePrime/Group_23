55fa07bb1b2ffde0bbe99fde60a93d85dd496b20
YuriSboarina <y.sboarina@studenti.unibg.it>
2023-05-31 17:35:54 +0200

    Fixed class AdjacentItemTiles

--- MyShelfie_Group23/src/player/AdjacentItemTiles.java
+++ MyShelfie_Group23/src/player/AdjacentItemTiles.java
@@ -7,9 +7,9 @@ import board.Card;
 
 public class AdjacentItemTiles {
 	
-	static Card matrixObj[][];
-	static int series;
-	static List<Integer> seriesArr = new ArrayList<Integer>(0);
+	private static Card matrixObj[][];
+	private static int series;
+	private static List<Integer> seriesArr = new ArrayList<Integer>(0);
 	
 	public static int CheckAdjacentItemTiles(Card matrix[][]) {
 		
@@ -28,7 +28,6 @@ public class AdjacentItemTiles {
 						checkNextRight(x,y,referenceCardColor);
 						checkNextDown(x,y,referenceCardColor);
 						seriesArr.add(series);
-						points = SumPoints();
 						
 					}
 						
@@ -43,48 +42,65 @@ public class AdjacentItemTiles {
 	
 	private static void checkNextUp(int x, int y, String referenceCardColor) {
 		
-		if(x-1 > 0 && matrixObj[x-1][y] != null && matrixObj[x-1][y].getColor() == referenceCardColor) {
-			matrixObj[x-1][y]= null;
-			series++;
-			checkNextLeft(x-1,y,referenceCardColor);
-			checkNextUp(x-1,y,referenceCardColor);
-			checkNextRight(x-1,y,referenceCardColor);
+		if(x-1 > 0 && matrixObj[x-1][y] != null) {
+			
+			if(referenceCardColor.equals(matrixObj[x-1][y].getColor())) {
+				matrixObj[x-1][y]= null;
+				series++;
+				checkNextLeft(x-1,y,referenceCardColor);
+				checkNextUp(x-1,y,referenceCardColor);
+				checkNextRight(x-1,y,referenceCardColor);
+			}
 		}
+		
 	}
 	
 	private static void checkNextDown(int x, int y, String referenceCardColor) {
 		
-		if(x+1 < 6 && matrixObj[x+1][y] != null && matrixObj[x+1][y].getColor() == referenceCardColor) {
-			matrixObj[x+1][y]= null;
-			series++;
-			checkNextRight(x+1,y,referenceCardColor);
-			checkNextDown(x+1,y,referenceCardColor);
-			checkNextLeft(x+1,y,referenceCardColor);
+		if(x+1 < 6 && matrixObj[x+1][y] != null){
+			
+			if(referenceCardColor.equals(matrixObj[x+1][y].getColor())) {
+				matrixObj[x+1][y]= null;
+				series++;
+				checkNextRight(x+1,y,referenceCardColor);
+				checkNextDown(x+1,y,referenceCardColor);
+				checkNextLeft(x+1,y,referenceCardColor);
+			}
 		}
+		
 	}
 	
 	private static void checkNextRight(int x, int y, String referenceCardColor) {
 		
-		if(y+1 < 5 && matrixObj[x][y+1] != null  && matrixObj[x][y+1].getColor() == referenceCardColor) {
-			matrixObj[x][y+1]= null;
-			series++;
-			checkNextRight(x,y+1,referenceCardColor);
-			checkNextDown(x,y+1,referenceCardColor);
-			checkNextUp(x,y+1,referenceCardColor);
-			checkNextLeft(x,y+1,referenceCardColor);
+		if(y+1 < 5  && matrixObj[x][y+1] != null) {
+		
+			if(referenceCardColor.equals(matrixObj[x][y+1].getColor())) {
+				matrixObj[x][y+1]= null;
+				series++;
+				checkNextRight(x,y+1,referenceCardColor);
+				checkNextDown(x,y+1,referenceCardColor);
+				checkNextUp(x,y+1,referenceCardColor);
+				checkNextLeft(x,y+1,referenceCardColor);
+			}
 		}
+		
 		
 	}
 	
 	private static void checkNextLeft(int x, int y, String referenceCardColor) {
 		
-		if(y-1 > 0 && matrixObj[x][y-1] != null && matrixObj[x][y-1].getColor() == referenceCardColor) {
-			matrixObj[x][y-1]= null;
-			series++;
-			checkNextLeft(x,y-1,referenceCardColor);
-			checkNextDown(x,y-1,referenceCardColor);
-			checkNextUp(x,y-1,referenceCardColor);
+		if(y-1 > 0 && matrixObj[x][y-1] != null ) {
+			
+			if(referenceCardColor.equals(matrixObj[x][y-1].getColor())) {
+				matrixObj[x][y-1]= null;
+				series++;
+				checkNextLeft(x,y-1,referenceCardColor);
+				checkNextDown(x,y-1,referenceCardColor);
+				checkNextUp(x,y-1,referenceCardColor);
+			}
+			
 		}
+		
 	}
 	
 	private static int SumPoints() {
