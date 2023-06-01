f419c0631bec44a0f25bcf5c41be0fde8861a44a
YuriSboarina <y.sboarina@studenti.unibg.it>
2023-06-01 16:33:35 +0200

    Fixed class AdjacentItemTiles

--- MyShelfie_Group23/src/player/AdjacentItemTiles.java
+++ MyShelfie_Group23/src/player/AdjacentItemTiles.java
@@ -24,6 +24,7 @@ public class AdjacentItemTiles {
 					
 						series = 1;
 						String referenceCardColor = matrixObj[x][y].getColor();
+						output();
 						matrixObj[x][y]= null;
 						checkNextRight(x,y,referenceCardColor);
 						checkNextDown(x,y,referenceCardColor);
@@ -42,10 +43,11 @@ public class AdjacentItemTiles {
 	
 	private static void checkNextUp(int x, int y, String referenceCardColor) {
 		
-		if(x-1 > 0 && matrixObj[x-1][y] != null) {
+		if(x-1 >= 0 && matrixObj[x-1][y] != null) {
 			
 			if(referenceCardColor.equals(matrixObj[x-1][y].getColor())) {
 				matrixObj[x-1][y]= null;
+				output();
 				series++;
 				checkNextLeft(x-1,y,referenceCardColor);
 				checkNextUp(x-1,y,referenceCardColor);
@@ -61,6 +63,7 @@ public class AdjacentItemTiles {
 			
 			if(referenceCardColor.equals(matrixObj[x+1][y].getColor())) {
 				matrixObj[x+1][y]= null;
+				output();
 				series++;
 				checkNextRight(x+1,y,referenceCardColor);
 				checkNextDown(x+1,y,referenceCardColor);
@@ -76,6 +79,7 @@ public class AdjacentItemTiles {
 		
 			if(referenceCardColor.equals(matrixObj[x][y+1].getColor())) {
 				matrixObj[x][y+1]= null;
+				output();
 				series++;
 				checkNextRight(x,y+1,referenceCardColor);
 				checkNextDown(x,y+1,referenceCardColor);
@@ -89,10 +93,11 @@ public class AdjacentItemTiles {
 	
 	private static void checkNextLeft(int x, int y, String referenceCardColor) {
 		
-		if(y-1 > 0 && matrixObj[x][y-1] != null ) {
+		if(y-1 >= 0 && matrixObj[x][y-1] != null ) {
 			
 			if(referenceCardColor.equals(matrixObj[x][y-1].getColor())) {
 				matrixObj[x][y-1]= null;
+				output();
 				series++;
 				checkNextLeft(x,y-1,referenceCardColor);
 				checkNextDown(x,y-1,referenceCardColor);
@@ -111,12 +116,47 @@ public class AdjacentItemTiles {
 		int p;
 		for(int i = 0; i < seriesArr.size(); i++) {
 			 p = seriesArr.get(i);
-			 if(p>7) {
+			 if(p>6) {
 				 p = 6;
 			 }
 			 points += pointsArray[p-1];
 		}
+		seriesArr.clear();
 		return points;
 	}
+	
+	public static void output()
+	{
+		System.out.println("\n\t   Columns\n\t1  2  3  4  5");
+		for (int i = 0; i < matrixObj.length; i++) {
+			System.out.print("\t");
+            for (int j = 0; j < matrixObj[i].length; j++) {
+            	if(matrixObj[i][j] == null) {
+            		System.out.print("\u001B[30m" + "*  " + "\u001B[0m");
+            	}else {
+            		
+            		 String tileColor = matrixObj[i][j].getColor();
+ 	                if(tileColor.equals("giallo")) {
+ 						System.out.print("\u001B[33m" + "#  " + "\u001B[0m");
+ 					}else if(tileColor.equals("rosso")) {
+ 						System.out.print("\u001B[31m" + "#  " + "\u001B[0m");
+ 					}else if(tileColor.equals("blu")) {
+ 						System.out.print("\u001B[34m" + "#  " + "\u001B[0m");
+ 					}else if(tileColor.equals("verde")) {
+ 						System.out.print("\u001B[32m" + "#  " + "\u001B[0m");
+ 					}else if(tileColor.equals("azzurro")) {
+ 						System.out.print("\u001B[36m" + "#  " + "\u001B[0m");
+ 					}else if(tileColor.equals("bianco")) {
+ 						System.out.print("\u001B[37m" + "#  " + "\u001B[0m");
+ 					}
+            		
+            	}
+	               
+            }
+            System.out.println();
+        }
+		System.out.println();
+	}
+
 
 }
